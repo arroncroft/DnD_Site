536472efc034051f1c11c9f09fd474dc379e4439
arroncroft <arroncroft@hotmail.com>
2018-05-08 22:24:42 -0400

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :factions
-  resources :characters
-  resources :regions
-  resources :places
+  root 'map#index', as: 'map_index'
+
   resources :party_members
+  resources :characters
+  resources :factions
+  resources :places
+  resources :regions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
